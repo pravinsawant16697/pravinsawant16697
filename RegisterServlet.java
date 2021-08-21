744ceb22dc6b015a510926275576efe959c3c071
pravinsawant16697 <83824231+pravinsawant16697@users.noreply.github.com>
2021-08-21 09:47:06 +0530

    Create RegisterServlet.java

---
+++ RegisterServlet.java
@@ -0,0 +1,58 @@
+package com.hibernate.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+import com.hibernate.entity.User;
+
+
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+   
+    public RegisterServlet() {
+        super();
+       
+    }
+
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String userName = request.getParameter("userName");
+		String password = request.getParameter("userPass");
+		String mobile = request.getParameter("mobile");
+		String email = request.getParameter("email");
+		System.out.println(userName+" "+password+" "+mobile+" "+email);
+		User user = new User(userName,password,email,mobile);
+		SessionFactory sf = new Configuration().configure().buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction transaction = session.beginTransaction();
+		session.save(user);
+		transaction.commit();
+		session.close();
+		sf.close();
+		System.out.println("User saved successfully!!!");
+		RequestDispatcher rd = request.getRequestDispatcher("List");
+		rd.forward(request,response);
+
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
